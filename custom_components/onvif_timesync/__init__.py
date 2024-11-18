790d3d606446c7c898e96fdf146c0c3670cd798f
Leaskovski <leaskovski@github>
2024-11-18 17:31:03 +0000

    Added config flows

--- custom_components/onvif_timesync/__init__.py
+++ custom_components/onvif_timesync/__init__.py
@@ -61,3 +61,14 @@ def setup(hass: HomeAssistant, config: ConfigEntry) -> bool:
 
     # Return boolean to indicate that initialization was successful.
     return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up from a config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
